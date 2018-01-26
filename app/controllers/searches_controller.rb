82b91eb02b260cfe4494dd6bffb95ce90628587a
zoebisch <zoebisch@gmail.com>
2018-01-26 15:07:30 -0500

    Done.

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,6 +1,16 @@
 class SearchesController < ApplicationController
   def search
   end
+  
+  def friends
+    resp = Faraday.get("https://api.foursquare.com/v2/users/self/friends") do |req|
+      req.params['oauth_token'] = session[:token]
+      # don't forget that pesky v param for versioning
+      req.params['v'] = '20160201'
+    end
+    @friends = JSON.parse(resp.body)["response"]["friends"]["items"]
+  end
+
 
   def foursquare
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root 'searches#search'
   get '/search', to: 'searches#search'
   post '/search', to: 'searches#foursquare'
-
+  get '/auth', to: 'sessions#create'
+  get '/friends', to: 'searches#friends'
 end
